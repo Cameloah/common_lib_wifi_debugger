9397cd90ab582d10c348ae19e440e81f303b6cf1
Cameloah <koor.jones@gmail.com>
2022-06-04 21:44:41 +0200

    OTA from github working

--- src/wifi_debugger.cpp
+++ src/wifi_debugger.cpp
@@ -5,36 +5,30 @@
 #include <WiFiClientSecure.h>
 #include "cert.h"
 #include "wifi_debugger.h"
+// #include "version.h"
 
 char url_fw_version[200] = "";
-char url_fw_bin[200] = "";
+String url_fw_bin;
+String fw_version;
 
-String FirmwareVer = {
-        "2.1"
-};
-
-
-void wifi_debugger_init(const char* user_ssid, const char* user_password, const char* url_version, const char* url_bin) {
+void
+wifi_debugger_init(const char *user_ssid, const char *user_password, const char *url_version, const char *url_bin) {
     // get all the user data first
     strcpy(url_fw_version, url_version);
-    strcpy(url_fw_bin, url_bin);
-    Serial.print("Active firmware version:");
-    Serial.println(FirmwareVer);
+    url_fw_bin = url_bin;
     connect_wifi(user_ssid, user_password);
 }
 
-void wifi_debugger_update() {
-}
-
 int timer_wifi_connect = 0;
-void connect_wifi(const char* ssid, const char* password) {
+
+void connect_wifi(const char *ssid, const char *password) {
     Serial.println("Warte auf WiFi");
     WiFi.begin(ssid, password);
     while ((WiFi.status() != WL_CONNECTED)) {
         delay(500);
         Serial.print(".");
         timer_wifi_connect++;
-        if(timer_wifi_connect > 60) {
+        if (timer_wifi_connect > 60) {
             Serial.println("Gespeichertes WiFi nicht gefunden.");
             return;
         }
@@ -48,14 +42,20 @@ void connect_wifi(const char* ssid, const char* password) {
 
 
 void wifi_debugger_firmwareUpdate(void) {
+    // initiate wifi update client
     WiFiClientSecure client;
     client.setCACert(rootCACertificate);
+    // github redirects you to the lastest version if you access ...\latest
     httpUpdate.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
-    t_httpUpdate_return ret = httpUpdate.update(client, "https://github.com/Cameloah/Kraeng-o-meter/releases/download/v1.0.1/kraengometer_v1_0_1.bin");
+    // lets update the given fw binary url with the latest version
+    url_fw_bin.replace("<version>", fw_version);
+    Serial.printf("\nDownloading and installing Firmware Version %s...\n", fw_version.c_str());
+    t_httpUpdate_return ret = httpUpdate.update(client, url_fw_bin);
 
     switch (ret) {
         case HTTP_UPDATE_FAILED:
-            Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+            Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n", httpUpdate.getLastError(),
+                          httpUpdate.getLastErrorString().c_str());
             break;
 
         case HTTP_UPDATE_NO_UPDATES:
@@ -67,39 +67,36 @@ void wifi_debugger_firmwareUpdate(void) {
             break;
     }
 }
-int wifi_debugger_fwVersionCheck(void) {
+
+int wifi_debugger_fwVersionCheck(uint8_t fw_major, uint8_t fw_minor, uint8_t fw_patch) {
     // if wifi not connected, cancel early
     if (WiFi.status() != WL_CONNECTED) {
         Serial.println("Wifi is not connected");
         return 0;
     }
 
-    String payload;
-    int httpCode;
-    String fwurl = "";
-    fwurl += "https://raw.githubusercontent.com/Cameloah/Kraeng-o-meter/master/bin_version.txt";
-    fwurl += "?";
-    fwurl += String(rand());
-    Serial.println(fwurl);
-    WiFiClientSecure * client = new WiFiClientSecure;
+    int httpCode;;
+    WiFiClientSecure *client = new WiFiClientSecure;
 
-    if (client)
-    {
-        client -> setCACert(rootCACertificate);
+    if (client) {
+        client->setCACert(rootCACertificate);
 
         // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is
         HTTPClient https;
 
-        if (https.begin( * client, fwurl))
-        { // HTTPS
-            Serial.print("[HTTPS] GET...\n");
+        if (https.begin(*client, url_fw_version)) {
             // start connection and send HTTP header
             delay(100);
+            // access the url
             httpCode = https.GET();
-            delay(100);
-            if (httpCode == HTTP_CODE_OK) // if version received
+
+            if (httpCode == HTTP_CODE_FOUND) // if version received
             {
-                payload = https.getString(); // save received version
+                // get updated url after redirect
+                String location = https.getLocation();
+                delay(100);
+                // lets extract the FW version from the url
+                fw_version = location.substring(location.indexOf("/releases/tag/") + 14);
             } else {
                 Serial.print("error in downloading version file:");
                 Serial.println(httpCode);
@@ -109,21 +106,31 @@ int wifi_debugger_fwVersionCheck(void) {
         delete client;
     }
 
-    if (httpCode == HTTP_CODE_OK) // if version received
+    if (httpCode == HTTP_CODE_FOUND) // if version received
     {
-        payload.trim();
+        fw_version.trim();
+        // extract firmware numbers
+        String fw_version_latest = fw_version;
+        uint8_t fw_latest_major = atoi(strtok((char *) fw_version_latest.c_str(), "v.\n"));
+        uint8_t fw_latest_minor = atoi(strtok(nullptr, "v.\n"));
+        uint8_t fw_latest_patch = atoi(strtok(nullptr, "v.\n"));
 
-        Serial.print("Firmware from Payload:");
-        if (payload.equals(FirmwareVer)) {
-            Serial.printf("\nDevice already on latest firmware version:%s\n", FirmwareVer.c_str());
+        if (fw_latest_major > fw_major) {
+            Serial.printf("\nNew Firmware detected! Major update available: v%d.%d.%d\n",
+                          fw_latest_major, fw_latest_minor, fw_latest_patch);
+        } else if (fw_latest_major == fw_major && fw_latest_minor > fw_minor) {
+            Serial.printf("\nNew Firmware detected! Minor update available: v%d.%d.%d\n",
+                          fw_latest_major, fw_latest_minor, fw_latest_patch);
+        } else if (fw_latest_major == fw_major && fw_latest_minor == fw_minor && fw_latest_patch > fw_patch) {
+            Serial.printf("\nNew Firmware detected! New Patch available: v%d.%d.%d\n",
+                          fw_latest_major, fw_latest_minor, fw_latest_patch);
+        } else {
+            Serial.printf("\nDevice already on latest or newer firmware version: v%d.%d.%d\n",
+                          fw_major, fw_minor, fw_patch);
             return 0;
         }
-        else
-        {
-            Serial.println(payload);
-            Serial.println("New firmware detected");
-            return 1;
-        }
+
+        return 1;
     }
     return 0;
 }
--- src/wifi_debugger.h
+++ src/wifi_debugger.h
@@ -7,7 +7,7 @@
 
 void connect_wifi(const char* ssid, const char* password);
 void wifi_debugger_firmwareUpdate();
-int wifi_debugger_fwVersionCheck();
+int wifi_debugger_fwVersionCheck(uint8_t fw_major, uint8_t fw_minor, uint8_t fw_patch);
 
 void wifi_debugger_init(const char* user_ssid, const char* user_password, const char* url_version, const char* url_bin);
 void wifi_debugger_update();
