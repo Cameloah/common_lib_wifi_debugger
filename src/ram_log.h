2eda543082c84c438ada4ce9c1823d81a486d052
Cameloah <koor.jones@gmail.com>
2023-04-02 12:45:49 +0200

    clean up, added doc strings

--- src/ram_log.h
+++ src/ram_log.h
@@ -7,7 +7,6 @@
 #include "Arduino.h"
 
 #define RAM_LOG_RINGBUFFER_LEN                  30
-#define RAM_LOG_PAYLOAD_SIZE                    4
 
 typedef enum{
     RAM_LOG_INFO                        = 0x00,
@@ -21,8 +20,16 @@ typedef struct {
     String payload;
 } ram_log_item_t;
 
-void ram_log_init();
-
+/// adds an entry to the ramlog
+/// \param itemtype what type of message
+/// \param user_payload error code
 void ram_log_notify(RAM_LOG_ITEM_t itemtype, uint32_t user_payload);
+
+/// adds an entry to the ramlog that can contain a sting
+/// \param itemtype what type of message
+/// \param user_payload message string
+/// \param flag_print when set to true, message string will also be printed in console
 void ram_log_notify(RAM_LOG_ITEM_t itemtype, const char* user_payload, bool flag_print = false);
+
+/// prints the entire ramlog and all entries nicely formatted
 void ram_log_print_log();
