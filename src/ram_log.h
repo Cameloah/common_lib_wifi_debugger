6ff9e770e76aaac1e9387c44658402c01e12c281
Cameloah <koor.jones@gmail.com>
2024-03-03 14:28:07 +0100

    added loop timer error

--- src/ram_log.h
+++ src/ram_log.h
@@ -13,7 +13,8 @@ typedef enum{
     RAM_LOG_ERROR_WIFI_HANDLER          = 0x01,
     RAM_LOG_ERROR_GITHUB_UPDATE         = 0x02,
     RAM_LOG_ERROR_MEMORY                = 0x03,
-    RAM_LOG_ERROR_GPS_MANAGER           = 0x04
+    RAM_LOG_ERROR_GPS_MANAGER           = 0x04,
+    RAM_LOG_ERROR_SYSTEM                = 0x05,
 } RAM_LOG_ITEM_t;
 
 typedef struct {
